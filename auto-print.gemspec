ffc7043fa47cb705607145cd25841745146e4dcd
alejandro londono <alondono11@gmail.com>
2015-12-15 15:43:30 -0500

    added error handling and flag options. getting closer to a CLI

--- auto-print.gemspec
+++ auto-print.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |s|
   s.name        = 'auto-print'
-  s.version     = '0.0.1'
+  s.version     = '0.1.0'
   s.date        = '2015-12-12'
   s.summary     = "auto-print"
   s.description = "automated image processing for url versioning in the erickson living corporation"
@@ -11,5 +11,5 @@ Gem::Specification.new do |s|
   s.executables << 'auto-print'
   s.add_runtime_dependency "selenium",  '~> 0.2.10', '>= 0.2.11'
   s.add_runtime_dependency "selenium-webdriver",  '~> 2.48', '>= 2.48.1'
-  
+  s.add_runtime_dependency "colorize",  '~> 0.0', '>= 0.7.7'
 end
--- lib/auto-print.rb
+++ lib/auto-print.rb
@@ -1,5 +1,6 @@
 require 'rubygems'
 require 'selenium-webdriver'
+require 'colorize'
 #################
 # COMMAND 
 #################
@@ -13,23 +14,82 @@ size = {
 	'xsmall' => 768,
 	'mobile' => 400
 }
-driver = Selenium::WebDriver.for :firefox
 
-com_codes.each do |com|
-	driver.navigate.to "#{url}"+"?from="+"#{com}".upcase
-	driver.save_screenshot("#{com}".upcase+".png")
-	puts "#{com}".upcase
-end
 
-size.each do |key, number|
-	driver.navigate.to "#{url}"
-	puts key
-	driver.manage.window.resize_to(number-50,800)
-	driver.save_screenshot("#{number}".upcase+".png")
+
+if ARGV.length == 1 and ARGV[0] == "help"
+	puts "
+		______________________
+		|                    |
+		|  A U T O P R I N T |
+		|                    |
+		|  ~~~~~~~~~~~~~~~~~ |
+		|  ~~~~~~~~~~~~~~~~~ |
+		|  ~~~~~~~~~~~~~~~~~ |
+		|  ~~~~~~~~~~~~~~~~~ |
+		|  ~~~~~~~~~~~~~~~~~ |
+		|  ~~~~~~~~~~~~~~~~~ |
+		L____________________|
+
+".cyan
+
+ puts "         THE AUTOMATED PRINTING PROGRAM ".yellow.blink
+
+puts "this program has been written to automate the process of preparing screenshots and printing them for strategy to pass their aproval. instead of taking hours to compile the necessary images, you can print them in seconds!
+
+this also makes things easier when uploading to NOOSH
+
+to run the program run this command:\n\n"
+
+puts "auto-print [url]\n".yellow
+
+print "where " 
+print " [url] ".yellow 
+puts "is the webpage you wish to make a screenshot\n\n"
+
+puts "make sure to not unclude any javascript variables in the url\n".red
+
+puts "you will be asked to answer a few questions about your project and then the necessary files will be created.
+make sure to delete any community webpages that were generated that are not apart of your campaign."
+
+
+elsif ARGV.length == 1 and (ARGV[0] == "-v" or ARGV[0] == "-version" or ARGV[0] == "version")
+
+	puts "
+				__________________________
+				| VERSION 0.1.0          |
+				--------------------------
+				".colorize(:yellow)
+	puts  " the use of this program is open source, but intended for the needs of Erickson Living employees only. if you wish to make a pull request you may do so at \n\n".yellow
+	puts  "https://github.com/alejandro-londono/auto-print".colorize(:color=>:white, :background=>:blue)
+	puts "\n\n"
+# --------------------------------------------
+# if the user input has nothing to do with the help or version options this code is run
+elsif ARGV.length == 1 and ARGV[0] != "version" or ARGV[0] != "help" or ARGV[0] != '-version' or ARGV[0] != "-v"  
+	# line 68 catches any user input that relates to .xls files in the form of 
+	# prune-erickson yourfile.xls [digit] outputfile.xls
+	if ARGV[0] =~ /(http)+[\S]*/
+		puts "program"
+		driver = Selenium::WebDriver.for :firefox
+		com_codes.each do |com|
+			driver.navigate.to "#{url}"+"?from="+"#{com}".upcase
+			driver.save_screenshot("#{com}".upcase+".png")
+			puts "#{com}".upcase
+		end
+
+		size.each do |key, number|
+			driver.navigate.to "#{url}"
+			puts key
+			driver.manage.window.resize_to(number-50,800)
+			driver.save_screenshot("#{number}".upcase+".png")
+		end
+		
+		
+	else
+		print "\n 
+		Are you trying to use auto-print? 
+		why dont you try this command:\n\n
+		auto-print help \n\n\n
+		"
+	end
 end
-# create error handling
-if ARGV[0] == "help" || ARGV[0] =="-help"
-	puts "help clause"
-elsif ARGV[0] == "version"
-	puts "Version 0.0.1"
-elsif
