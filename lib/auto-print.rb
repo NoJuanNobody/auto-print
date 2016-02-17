6911d86404a6fb16ae6abdc75685f654fcce4f9d
Alejandro Londonogit config --global user.email alondono11@gmail.comcdgit config --;istgit config --global user.name Alejandro <alondono11@gmail.com>
2016-02-17 17:34:14 -0500

    adding branch for brittany's preferences

--- lib/auto-print.rb
+++ lib/auto-print.rb
@@ -141,7 +141,7 @@ elsif ARGV.length == 1 and ARGV[0] != "version" or ARGV[0] != "help" or ARGV[0] 
 			driver = Selenium::WebDriver.for :firefox
 			com_codes.each do |com|
 				# printing email CS
-				driver.navigate.to "#{url}"+"?from="+"#{com}-email".upcase
+				driver.navigate.to "#{url}"+"&from="+"#{com}-email".upcase
 				driver.save_screenshot("#{com}".upcase+"-email.png")
 				print "#{com}-EMAIL   ".upcase
 			end	
@@ -154,7 +154,7 @@ elsif ARGV.length == 1 and ARGV[0] != "version" or ARGV[0] != "help" or ARGV[0] 
 			driver = Selenium::WebDriver.for :firefox
 			com_codes.each do |com|
 				# landing pages
-				driver.navigate.to "#{url}"+"?from="+"#{com}".upcase
+				driver.navigate.to "#{url}"+"&from="+"#{com}".upcase
 				driver.save_screenshot("#{com}".upcase+".png")
 				print "#{com}   ".upcase
 			end
